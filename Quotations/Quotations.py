8d6870ebf94d86d97a5fae15850cc976e4219007
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-14 15:07:03 +0530

    Add files via upload

---
+++ Quotations/Quotations.py
@@ -0,0 +1,15 @@
+#using single quotes
+print('Hello Python World')
+
+#using double quotes
+print("Hello Python World")
+
+#single quotes inside double quotes
+print("It's a sunny day")
+
+#double quotes inside single quotes
+print('He said "Python is fun"')
+
+#Escaping quotes
+print('It\'s a sunny day')  
+print("He said \"Python is fun\"")  
