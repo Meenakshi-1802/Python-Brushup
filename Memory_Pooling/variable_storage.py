21095f820c7f6aa86f248ec8fbd62127ec4799e5
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-14 20:11:22 +0530

    Add files via upload

---
+++ Memory_Pooling/variable_storage.py
@@ -0,0 +1,16 @@
+#Basic Variable Storage
+emp_id = 1001
+emp_name = "SCOTT"
+emp_job = "MANAGER"
+emp_salary = 9550.50
+emp_dept_name = "admin"
+emp_gender = "male"
+emp_age = 35
+
+print(emp_id)
+print(emp_name)
+print(emp_job)
+print(emp_salary)
+print(emp_dept_name)
+print(emp_gender)
+print(emp_age)
