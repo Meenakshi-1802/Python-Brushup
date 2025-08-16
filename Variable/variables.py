1e0a4bc260f1ee0bf3876300dd1a1a637125ee33
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-16 22:56:09 +0530

    Add files via upload

---
+++ Variable/variables.py
@@ -0,0 +1,18 @@
+empNum = 1001
+empName ='SCOTT'
+empJob = 'MANAGER'
+empSalary = 20000.50
+empDeptName = 'SALES'
+empGender = 'Male'
+empAge = 35
+
+print(empNum )
+print(empName)
+print(empJob)
+print(empSalary)
+print(empDeptName)
+print(empGender)
+print(empAge)
+
+#print in same line
+print(empNum,empName,empJob,empSalary,empDeptName,empGender,empAge)
