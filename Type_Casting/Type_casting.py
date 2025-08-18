22f42374ebc6e6ab3bd982e1bde017f0a045dafc
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-18 21:48:14 +0530

    Add files via upload

---
+++ Type_Casting/Type_casting.py
@@ -0,0 +1,53 @@
+x = int(10)
+type(x) #x will be 10
+print(x)
+
+y = int(10.5)
+type(y) #y will be 10.5
+print(y)
+
+z = int('10')
+type(z) #z will be 10
+print(z)
+
+x = int(True)
+type(x) #x will be 1
+print(x)
+
+y = int(False)
+type(y) #y will be 0
+print(y)
+ 
+a = float(1)
+type(a) #a will be 1.0
+print(a)
+
+b = float(2.8)
+type(b) #b will be 2.8
+print(b)
+
+c = float('3')
+type(c) #c will be 3.0
+print(c)
+
+d = float('4.2')
+type(d) #d will be 4.2
+print(d)
+
+x = str("s1")
+type(x) #x will be s1
+print(x)
+
+y = str(2)
+type(y) #y will be 2
+print(y)
+
+z = str(3.0)
+type(z) #z will be 3.0
+print(z)
+
+a = 10
+b = 5
+z = complex(a,b)
+type(z) #z will be (10+5j)
+print(z)
