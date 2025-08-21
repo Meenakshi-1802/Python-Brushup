d19ac78d45293db4baa12506b748b6be048aa102
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-21 21:22:27 +0530

    Add files via upload

---
+++ Operators/assignment_operator.py
@@ -0,0 +1,8 @@
+# Using assignment operator
+x = 10     
+name = "Meenakshi"  
+is_active = True    
+
+print("x =", x)
+print("name =", name)
+print("is_active =", is_active)
---
+++ Operators/shorthand_assignment.py
@@ -0,0 +1,12 @@
+x = 5
+print("x =", x)
+x += 3   # x = x + 3
+print("x += 3 →", x)
+x -= 2   # x = x - 2
+print("x -= 2 →", x)
+x *= 4   # x = x * 4
+print("x *= 4 →", x)
+x //= 3  # x = x // 3
+print("x //= 3 →", x)
+x **= 2  # x = x ** 2
+print("x **= 2 →", x)
