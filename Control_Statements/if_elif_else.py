0a5bacd4f95db463e14d36f3377996bdcb13ed9f
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-23 23:37:47 +0530

    Add files via upload

---
+++ Control_Statements/if_elif_else.py
@@ -0,0 +1,10 @@
+a = int(input("Enter First number : "))
+b = int(input("Enter second number : "))
+if a == b:
+    print("Equal Numbers")
+
+elif a > b: 
+    print("Greater Number",a)
+
+else:
+    print("Greater Number",b)
