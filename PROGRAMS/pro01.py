ec08b0a70fb5daba0fe2069a417c01f7bbbb4bdd
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-23 23:45:15 +0530

    Add files via upload

---
+++ PROGRAMS/pro01.py
@@ -0,0 +1,9 @@
+## Program to find Greater of Two Numbers:
+a = int(input("Enter First number : "))
+b = int(input("Enter second number : "))
+
+if a > b: 
+    print("Greater Number",a)
+
+else:
+    print("Greater Number",b)
---
+++ PROGRAMS/pro02.py
@@ -0,0 +1,11 @@
+## Program to find Equal & Greater of Two Numbers:
+a = int(input("Enter First number : "))
+b = int(input("Enter second number : "))
+if a == b:
+    print("Equal Numbers")
+
+elif a > b: 
+    print("Greater Number",a)
+
+else:
+    print("Greater Number",b)
