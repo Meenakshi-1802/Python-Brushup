66287336d1ef9b5e99e692212cb6a8aa8d1f21aa
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-24 12:51:08 +0530

    Add files via upload

---
+++ PROGRAMS/pro08.py
@@ -0,0 +1,17 @@
+## Program to find subject wise Result and Final Result
+m1 = int(input("Enter sub1 Marks :"))
+m2 = int(input("Enter sub2 Marks :"))
+m3 = int(input("Enter sub3 Marks :"))
+
+if m1>=40:
+    print("sub1 Pass")
+else:
+    print("sub1 Fail")
+if m2>=40:
+    print("sub2 Pass")
+else:
+    print("sub2 Fail")
+if m3>= 40:
+    print("sub3 Pass")
+else:
+    print("sub3 Fail")
