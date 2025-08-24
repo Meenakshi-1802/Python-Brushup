92c9257b485532d19f5dda44d172a652526c4a8d
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-24 11:25:38 +0530

    Add files via upload

---
+++ PROGRAMS/pro07.py
@@ -0,0 +1,9 @@
+## Program to check the result of the students:
+m1 = int(input("enter sub1 marks :"))
+m2 = int(input("enter sub2 marks :"))
+m3 = int(input("enter sub3 marks :"))
+
+if m1>=40 and m2>=40 and m3>=40:
+    print("Pass")
+else:
+    print("fail")
