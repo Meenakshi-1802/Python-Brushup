5dee09f2f66d230d055d7f89830da1a052021cb2
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-24 14:51:40 +0530

    Add files via upload

---
+++ PROGRAMS/pro12.py
@@ -0,0 +1,6 @@
+## Leap Year
+year = int(input("Enter the year:"))
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print("It is a Leap year")
+else:
+    print("It is not a leap year")
