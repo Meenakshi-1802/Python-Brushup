c8585ab3bf67631d4d8cf850eb1c0939462471ce
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-08-25 19:09:40 +0530

    Add files via upload

---
+++ mathemetical_functions/mathematical_function.py
@@ -0,0 +1,24 @@
+import math
+res = math.fabs(-10) #absolute function
+print(res)
+
+res = math.sqrt(16) #Square root function
+print(res)
+
+res = math.pow(10,2) #power function
+print(res)
+
+res = math.fmod(10,3) #mod function
+print(res)
+
+res = math.floor(10.9) #floor function
+print(res)
+
+res = math.ceil(10.1) #ceil function
+print(res)
+
+res = math.trunc(10.9)
+print(res)
+
+res = math.factorial(5)
+print(res)
