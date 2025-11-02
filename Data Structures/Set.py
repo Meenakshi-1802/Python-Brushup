6572d373aff552822bbaecefcfcdadd57f15921b
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-11-02 22:36:31 +0530

    Add files via upload

---
+++ Data Structures/Set.py
@@ -0,0 +1,15 @@
+set = {1,2,3,5,7,5,8}
+set.add(9)
+print(set)
+set.remove(5)
+print(set)
+set.pop()
+print(set)
+set.clear()
+print(set)
+
+#Methods
+set1 = {1,2,3,4}
+set2 = {3,4,5,6}
+print(set1.union(set2))
+print(set1.intersection(set2))
