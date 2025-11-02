3fab984bf323be9c1ac8c2fcc340c89052c54a74
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-11-02 19:14:07 +0530

    Add files via upload

---
+++ PROGRAMS/13pro.py
@@ -0,0 +1,7 @@
+#WAP to input user's first name & print its length
+name = input("Enter First Name:")
+print(len(name))
+
+#WAP to find the occurrence of '$' in a string
+string = "I have 20$ in my wallet"
+print(string.count("$"))
---
+++ PROGRAMS/string_func.py
@@ -0,0 +1,11 @@
+string = "Python is a high level programming language"
+
+print(string.endswith("ge"))
+
+print(string.capitalize())
+
+print(string.replace("high","low"))
+
+print(string.find("a"))
+
+print(string.count("l"))
