5bfa64b3e961a220832e44134cd12eae77f81f93
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-11-02 11:47:12 +0530

    Add files via upload

---
+++ PROGRAMS/Ternary.py
@@ -0,0 +1,6 @@
+"""food = input("Enter Food:")
+eat = "yes" if food == "cake" else "No"
+print(eat)"""
+
+food = input("food:")
+print("sweet") if food == "cake" or food == "jalebi" else print("not sweet ")
