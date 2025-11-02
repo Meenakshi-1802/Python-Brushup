394c5e0daf64f7a001fc2bd81f684b728d0f88c0
MEENAKSHI PALAI <pmeenakshi1802@gmail.com>
2025-11-02 22:11:22 +0530

    Add files via upload

---
+++ Data Structures/Dictionary.py
@@ -0,0 +1,12 @@
+info = {
+    "name" : "Meenakshi",
+    "class" : "MCA",
+    "age" : 22,
+    "marks" : 80
+}
+print(info.keys())
+print(info.values())
+print(info.items())
+print(info.get("key"))
+info.update({"city" : "Delhi"})
+print(info)
